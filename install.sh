3f928da62cdef411efd8a4c4d7812e94bece977c
Ionut-Vlad Modoranu <modoranu.ionut.vlad@hotmail.com>
2024-05-27 00:16:54 +0200

    added conda init command

--- install.sh
+++ install.sh
@@ -2,17 +2,20 @@
 
 ENV_NAME=ista_daslab_optimizers
 
-echo "Loading CUDA 12.2 module"
+echo ">>>>> Loading CUDA 12.2 module"
 module load cuda/12.2
 
-echo "Creating environment \"${ENV_NAME}\""
+echo ">>>>> Creating environment \"${ENV_NAME}\""
 conda create --name $ENV_NAME python=3.9 -y
 
-echo "Activating environment"
+echo ">>>>> Running conda init..."
+conda init bash
+
+echo ">>>>> Activating environment"
 conda activate $ENV_NAME
 
-echo "Installing packages..."
+echo ">>>>> Installing packages..."
 pip3 install torch torchvision torchaudio wandb gpustat
 
-echo "Installing packages..."
+echo ">>>>> Installing packages..."
 pip3 install .
