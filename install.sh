0a5a0777979698218dc0afaf90de0c4649c38db4
Ionut-Vlad Modoranu <modoranu.ionut.vlad@hotmail.com>
2024-05-27 00:11:19 +0200

    added messages

--- install.sh
+++ install.sh
@@ -1,11 +1,22 @@
 #!/bin/bash
 
+ENV_NAME=ista_daslab_optimizers
+
+echo "Loading CUDA 12.2 module"
 module load cuda/12.2
-conda create --name ista_daslab_optimizers python=3.9 -y
-conda activate ista_daslab_optimizers
-pip3 install torch torchvision torchaudio
-pip3 install wandb gpustat
+
+echo "Creating environment \"${ENV_NAME}\""
+conda create --name $ENV_NAME python=3.9 -y
+
+echo "Activating environment"
+conda activate $ENV_NAME
+
+echo "Installing packages..."
+pip3 install torch torchvision torchaudio wandb gpustat
+
 cd ~
 git clone git@github.com:IST-DASLab/ISTA-DASLab-Optimizers.git
 cd ISTA-DASLab-Optimizers
+
+echo "Installing packages..."
 pip3 install .
