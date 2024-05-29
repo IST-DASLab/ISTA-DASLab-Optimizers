4d6832d4129578a102793514ba8540042f80d2e4
Ionut-Vlad Modoranu <modoranu.ionut.vlad@hotmail.com>
2024-05-29 13:53:36 +0200

    added run_example script

---
+++ run_example.sh
@@ -0,0 +1,12 @@
+CUDA_VISIBLE_DEVICES=0 python example.py \
+    --model rn18 \
+    --dataset_path ./data \
+    --dataset_name cifar10 \
+    --optimizer micro-adam \
+    --epochs 100 \
+    --batch_size 128 \
+    --learning_rate 1e-3 \
+    --m 10 \
+    --k 0.01 \
+    --ef_quant_bucket_size 32 \
+    --precision bf16
