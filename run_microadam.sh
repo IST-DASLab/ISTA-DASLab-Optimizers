b3bd21e831e2a2a18946b4f129b8edcee11ce9ff
Ionut-Vlad Modoranu <modoranu.ionut.vlad@hotmail.com>
2024-05-29 15:52:30 +0200

    renamed run_example to run_microadam

--- run_microadam.sh
+++ run_microadam.sh
@@ -1,12 +1,22 @@
+clear
+
 CUDA_VISIBLE_DEVICES=0 python example.py \
     --model rn18 \
     --dataset_path ./data \
     --dataset_name cifar10 \
     --optimizer micro-adam \
+    \
     --epochs 100 \
     --batch_size 128 \
-    --learning_rate 1e-3 \
+    \
+    --lr 1e-3 \
     --m 10 \
     --k 0.01 \
     --ef_quant_bucket_size 32 \
-    --precision bf16
+    \
+    --precision bf16 \
+    \
+    --wandb_project ista-daslab-optimizers \
+    --wandb_group example \
+    --wandb_job_type cifar10 \
+    --wandb_name micro-adam
