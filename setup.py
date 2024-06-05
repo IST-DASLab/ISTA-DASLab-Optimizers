be45aa63a4077da8b853f08c730cf64e1ee11f84
Ionut-Vlad Modoranu <modoranu.ionut.vlad@hotmail.com>
2024-06-05 21:40:26 +0200

    all optimizers are installable via pip

--- setup.py
+++ setup.py
@@ -19,7 +19,6 @@ setup(
             sources=[
                 'kernels/cuda_daslab_tools/daslab_tools.cpp',
                 'kernels/cuda_daslab_tools/daslab_tools.cu',
-                # 'kernels/utils.cu',
             ],
             extra_link_flags=['--diag-suppress 20050', '--diag-suppress 816']),
         CUDAExtension(
@@ -29,24 +28,20 @@ setup(
                 'kernels/cuda_micro_adam/microadam_update.cu',
                 'kernels/cuda_micro_adam/microadam_asymm_block_quant.cu',
                 'kernels/cuda_micro_adam/microadam_asymm_block_quant_inv.cu',
-                # 'kernels/utils.cu',
             ]),
-            # extra_compile_args=['-diag-suppress 20050']),
-        # CUDAExtension(
-        #     name=f'cuda_dense_mfac_sm{cc[0]}{cc[1]}',
-        #     sources=[
-        #         'kernels/cuda_dense_mfac/densemfac.cpp',
-        #         'kernels/cuda_dense_mfac/densemfac_kernel.cu',
-        #     ],
-        #     extra_compile_args=['-diag-suppress 20050-D']),
-        # CUDAExtension(
-        #     name=f'cuda_sparse_mfac_sm{cc[0]}{cc[1]}',
-        #     sources=[
-        #         'kernels/cuda_sparse_mfac/sparsemfac.cpp',
-        #         'kernels/cuda_sparse_mfac/sparsemfac_SP_kernel.cu',
-        #         'kernels/cuda_sparse_mfac/sparsemfac_LCG_kernel.cu',
-        #     ],
-        #     extra_compile_args=['-diag-suppress 20050-D']),
+        CUDAExtension(
+            name=f'cuda_sparse_mfac_sm{cc[0]}{cc[1]}',
+            sources=[
+                'kernels/cuda_sparse_mfac/sparsemfac.cpp',
+                'kernels/cuda_sparse_mfac/sparsemfac_SP_kernel.cu',
+                'kernels/cuda_sparse_mfac/sparsemfac_LCG_kernel.cu',
+            ]),
+        CUDAExtension(
+            name=f'cuda_dense_mfac_sm{cc[0]}{cc[1]}',
+            sources=[
+                'kernels/cuda_dense_mfac/densemfac.cpp',
+                'kernels/cuda_dense_mfac/densemfac_kernel.cu',
+            ]),
     ],
     cmdclass={'build_ext': BuildExtension}
 )
