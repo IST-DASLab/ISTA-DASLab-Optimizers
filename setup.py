d593c4a0c67dc1f78acce12b7da1b3624a988f32
Ionut-Vlad Modoranu <modoranu.ionut.vlad@hotmail.com>
2024-06-10 12:58:30 +0200

    added working version of setup.py

--- setup.py
+++ setup.py
@@ -1,87 +1,43 @@
 from setuptools import setup, find_packages
 from torch.utils.cpp_extension import CUDAExtension, BuildExtension
-import pybind11
-import torch
-import os
-
-cc = torch.cuda.get_device_capability(0)
-cc = f'{cc[0]}{cc[1]}'
-CUDA_HOME = os.environ['CUDA_HOME']
-
-def get_cuda_extension(name, sources):
-    return CUDAExtension(
-        # name=name,
-        name=f'ista_daslab_optimizers.{name}',
-        sources=sources,
-        # dlink=True,
-        # dlink_libraries=['dlink_lib'],
-        extra_compile_args={
-            'cxx': [],
-            'nvcc': [
-                f'-arch=sm_{cc}',
-                '-lcublas',
-                '-lcudart',
-                '-O3',
-                # f'--default-stream per-thread',
-                f'--output-directory=build/temp.linux-x86_64-cpython-39/{name}/'
-            ],
-            'nvcclink': [
-                f'-arch=sm_{cc}',
-                '--device-link'
-            ]
-        },
-        include_dirs=[os.path.abspath('./kernels'), pybind11.get_include()],
-        library_dirs=[os.path.join(CUDA_HOME, 'lib64')],
-        # libraries=['cusparse', 'cublas'],
-    )
 
 setup(
     name='ista_daslab_optimizers',
     version='0.0.1',
     author='Ionut-Vlad Modoranu',
     author_email='ionut-vlad.modoranu@ist.ac.at',
-    description='Deep Learning optimizers developed in the Distributed Algorithms and Systems group '
-                '(DASLab) @ Institute of Science and Technology Austria (ISTA).',
+    description='Deep Learning optimizers developed in the Distributed Algorithms and Systems group (DASLab) @ Institute of Science and Technology Austria (ISTA)',
     url='https://github.com/IST-DASLab/ISTA-DASLab-Optimizers',
-
+    
     packages=find_packages(),
-    # install_requires=['torch', 'pybind11', 'ninja'],
     cmdclass={'build_ext': BuildExtension},
 
     ext_modules=[
-        get_cuda_extension(
-            name=f'cuda_daslab_tools_sm{cc}',
+        CUDAExtension(
+            name=f'ista_daslab_tools',
             sources=[
-                'kernels/cuda_daslab_tools/daslab_tools.cpp',
-                'kernels/cuda_daslab_tools/daslab_tools.cu',
+                'kernels/tools/tools.cpp',
+                'kernels/tools/tools_kernel.cu',
             ]),
-        # get_cuda_extension(
-        #     name=f'cuda_micro_adam_sm{cc}',
-        #     sources=[
-        #         'kernels/cuda_micro_adam/microadam.cpp',
-        #         'kernels/cuda_micro_adam/microadam_update.cu',
-        #         'kernels/cuda_micro_adam/microadam_asymm_block_quant.cu',
-        #         'kernels/cuda_micro_adam/microadam_asymm_block_quant_inv.cu',
-        #     ]),
-        # get_cuda_extension(
-        #     name=f'cuda_sparse_mfac_sm{cc}',
-        #     sources=[
-        #         'kernels/cuda_sparse_mfac/sparsemfac.cpp',
-        #         'kernels/cuda_sparse_mfac/sparsemfac_SP_kernel.cu',
-        #         'kernels/cuda_sparse_mfac/sparsemfac_LCG_kernel.cu',
-        #     ]),
-        # get_cuda_extension(
-        #     name=f'cuda_dense_mfac_sm{cc}',
-        #     sources=[
-        #         'kernels/cuda_dense_mfac/densemfac.cpp',
-        #         'kernels/cuda_dense_mfac/densemfac_kernel.cu',
-        #     ]),
-    ]
-)
-
-# extra_link_flags=['--diag-suppress 20050', '--diag-suppress 816']),
-# include_dirs = [pybind11.get_include()],
-# language = 'c++',
-# extra_compile_args = ['-std=c++17', '-lcudart', '-lcuda'],
-# extra_link_args = [f'-L{CUDA_HOME}']
-# install_requires=['pybind11'],
+        CUDAExtension(
+            name=f'ista_daslab_dense_mfac',
+            sources=[
+                'kernels/dense_mfac/dense_mfac.cpp',
+                'kernels/dense_mfac/dense_mfac_kernel.cu',
+            ]),
+        CUDAExtension(
+            name=f'ista_daslab_sparse_mfac',
+            sources=[
+                'kernels/sparse_mfac/sparse_mfac.cpp',
+                'kernels/sparse_mfac/sparse_mfac_SP_kernel.cu',
+                'kernels/sparse_mfac/sparse_mfac_LCG_kernel.cu',
+            ]),
+        CUDAExtension(
+            name=f'ista_daslab_micro_adam',
+            sources=[
+                'kernels/micro_adam/micro_adam.cpp',
+                'kernels/micro_adam/micro_adam_update.cu',
+                'kernels/micro_adam/micro_adam_asymm_block_quant.cu',
+                'kernels/micro_adam/micro_adam_asymm_block_quant_inv.cu',
+            ]),
+    ])
