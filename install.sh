4b8c12a0ae6422eccdffd2149f9d17d21fc1fc98
Ionut-Vlad Modoranu <modoranu.ionut.vlad@hotmail.com>
2024-06-12 18:41:34 +0200

    updated install script

--- install.sh
+++ install.sh
@@ -1,4 +1,4 @@
-ENV_NAME=ista_from_scratch
+ENV_NAME=ista
 
 #echo ">>>>> Loading CUDA 12.2 module"
 #module load cuda/12.2
@@ -7,9 +7,10 @@ echo ">>>>> Creating environment \"${ENV_NAME}\""
 conda create --name $ENV_NAME python=3.9 -y
 conda activate $ENV_NAME
 
-echo ">>>>> Installing required packages..."
-pip3 install torch torchvision torchaudio --index-url https://download.pytorch.org/whl/cu121
-pip3 install wandb gpustat timm einops psutil
+#echo ">>>>> Installing required packages..."
+#pip3 install torch torchvision torchaudio --index-url https://download.pytorch.org/whl/cu121
+#pip3 install wandb gpustat timm einops psutil
 
 echo ">>>>> Installing ISTA-DASLab-Optimizers..."
+rm -rf build dist *egg* # delete files from previous installations
 pip3 install .
