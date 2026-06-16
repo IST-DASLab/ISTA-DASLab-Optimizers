ee75ad65a587a538d34ebf6fcfc362f68d1e9e5e
Ionut-Vlad Modoranu <modoranu.ionut.vlad@hotmail.com>
2026-06-16 16:27:23 +0200

    added damping to the input matrix before computing scaling: useful for modded-nanogpt where the gradient can be completely zero

--- ista_daslab_optimizers/dash/invertors/scalers.py
+++ ista_daslab_optimizers/dash/invertors/scalers.py
@@ -8,25 +8,35 @@ from ..types import DashMatrixScalingType
 class DashMatrixScaling:
     @staticmethod
     @torch.no_grad()
-    def get_matrix_scaling(A: Tensor, cfg: DashConfig):
+    def get_matrix_scaling(A: Tensor, cfg: DashConfig, eps: float=1e-8):
         scaling_type = cfg.matrix_scaling_type
 
-        match scaling_type:
-            case DashMatrixScalingType.POWER_ITER:
-                scale, _ = DashMatrixScaling.max_eigval_power_iter(A, num_iters=cfg.matrix_scaling_pi_steps)
-                return scale * cfg.matrix_scaling_const # multiply by the constant here
-            case DashMatrixScalingType.POWER_ITER_MULTI:
-                A16 = A.bfloat16()
-                scale, _ = DashMatrixScaling.max_eigval_power_iter_multi(A16, num_iters=cfg.matrix_scaling_pi_steps, num_vecs=16)
-                # if torch.isnan(scale).sum() > 0:
-                #     print(f'Found NaN in PIM-scaling!!!')
-                del A16
-                return scale * cfg.matrix_scaling_const # multiply by the constant here
-            case DashMatrixScalingType.FRO:
-                scale = A.norm(p='fro', dim=(1, 2), keepdim=True)
-                return scale # do not scale the frobenius norm by the constant set by user
-            case _:
-                raise RuntimeError(f'Received unknown NDBScalingType: {scaling_type}')
+        B, N, _ = A.shape
+        idx = torch.arange(N).to(A.device)
+        A16 = A.bfloat16()
+
+        ##### START EXPLANATION FOR DAMPING
+        """
+            We need to dampen the bf16 version of A because in modded-nanogpt some layers have gradient that is completely zero at the first step.
+            When inputting a zero matrix block DASH invertor, the power iteration performs 0/0 operation which results in NaNs.
+            These NaNs will then propagate to the model and therefore at the second optimization step the loss became NaN. 
+        """
+        A16[:, idx, idx] += eps
+        ##### END EXPLANATION FOR DAMPING
+
+        if scaling_type == DashMatrixScalingType.POWER_ITER:
+            scale, _ = DashMatrixScaling.max_eigval_power_iter(A16, num_iters=cfg.matrix_scaling_pi_steps)
+            multiplier = cfg.matrix_scaling_const
+        elif scaling_type == DashMatrixScalingType.POWER_ITER_MULTI:
+            scale, _ = DashMatrixScaling.max_eigval_power_iter_multi(A16, num_iters=cfg.matrix_scaling_pi_steps, num_vecs=16)
+            multiplier = cfg.matrix_scaling_const
+        elif scaling_type == DashMatrixScalingType.FRO:
+            scale = A.norm(p='fro', dim=(1, 2), keepdim=True)
+            multiplier = 1
+        else:
+            raise RuntimeError(f'Received unknown NDBScalingType: {scaling_type}')
+
+        return scale * multiplier
 
     @staticmethod
     @torch.no_grad()
@@ -69,7 +79,7 @@ class DashMatrixScaling:
 
         bmm(A, v, out=Av)
         eig_vals_all = (v * Av).sum(dim=1) # (N, B, num_vecs).sum(dim=1) => (N, num_vecs)
-
+        print(eig_vals_all)
         max_vals, max_indices = eig_vals_all.max(dim=1) # (N, num_vecs).max(dim=1) => (N,)
 
         idx_expanded = max_indices.view(N, 1, 1).expand(N, B, 1) # (N, B, 1)
